8dba4314b083b3fe1aca551b7d0ff6771f3624e3
System Administrator <root@D-69-91-160-6.dhcp4.washington.edu>
2015-07-13 15:05:58 -0700

    note file

---
+++ 0713ex.py
@@ -0,0 +1,71 @@
+myName = 'Zed'
+my_age = 35 # not a lie
+my_height = 74.7 # inches
+my_weight = 180 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print "Let's talk about %s." % myName
+print "Let's talk about",  myName
+print "my height is", round(my_height, 1)
+
+#************************************************************ EX 6
+
+string1 = "this is the first part"
+string2 = "of the second part"
+print string1 + string2
+print string1, string2
+
+#************************************************************ EX 7
+
+print "Mary had a little lamb."
+print "Its fleece was white as %s." % "snow"
+print "And everywhere that Marry went."
+print "." * 10 # print out 10 "*" ?
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end.  try removing it to see what happens
+print end1 + end2 + end3 + end4 + end5 + end6
+print end7 + end8 + end9 + end10 + end11 + end12
+
+#************************************************************ EX 8
+
+formatter = "%r %r %r %r"
+
+print formatter % ("one", "two", "\nthree", 'four')
+print 'this is a string'
+
+#************************************************************ EX 9
+
+print "print double quote \
+	this way"
+print """ or print
+	double
+	quote
+	this 
+	way"""
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+print "Here are the days: ", days
+print "Here are the days: " + days
+
+#************************************************************ EX 10
+
+while True:
+    for i in ["/","-","|","\\","|"]:
+	        print "%s\r" % i,
+
+
---
+++ ex1.py
@@ -0,0 +1,13 @@
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+addition = "%i, %i" % (3+7,  5+4)
+print addition
+print 'try "single" quote'
+print 3+4,\
+	7+9
+print 3 == 4
